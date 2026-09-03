091101a54c3e985bf38a027a29177d6dff09dd0b
mridul-bindal <mridulbindal4@gmail.com>
2026-09-04 01:30:27 +0530

    added the logic of inverted index

--- main.py
+++ main.py
@@ -16,6 +16,16 @@ def load_files(path :str) -> dict[str, str]:
                 filedata[file] = f.read()
     return filedata
 
+def build_inverted_index(file_data: dict[str, str]) -> dict[str, set[str]]:
+    inverted_index = {}
+
+    for file, file_content in file_data.items():
+        tokens = set(tockenize(file_content))
+        for token in tokens:
+            inverted_index.setdefault(token, set()).add(file)
+
+    return inverted_index
+ 
 def basic_search(query : str, fileData: dict[str, str]) -> list[str]:
     tokens = set(tockenize(query))
     results = []
@@ -31,12 +41,28 @@ def measure_time(func, *args):
     end=time.perf_counter()
     return result,end-start
 
+
+def  search_with_inverted_index(query:str, inverted_index:dict[str, set[str]]) -> list[str]:
+    tokens = set(tockenize(query))
+    results = set()
+    for token in tokens:
+        if token in inverted_index:
+            results.update(inverted_index[token])
+    return list(results)
+
 def main():
     fileData=load_files("data")
-    query=" Computer programming course"
+    query="mongoDB database"
     result,time=measure_time(basic_search,query,fileData)
     print(result)
     print(time)
+    inverted_index=build_inverted_index(fileData)
+    # print(inverted_index.keys())
+    result2,time2=measure_time(search_with_inverted_index,query,inverted_index)
+    print(result2)
+    print(time2)
+
+
 
 if __name__ == "__main__":
     main()
